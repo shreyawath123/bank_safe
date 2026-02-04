5aa4cd2023393f5f1dc8a2f774741ff65e7a22a0
shreyawath123 <shreyawath123@gmail.com>
2026-02-04 11:35:01 +0530

    Update DatabaseConnectionManager.java

--- server/src/main/java/com/edutech/progressive/config/DatabaseConnectionManager.java
+++ server/src/main/java/com/edutech/progressive/config/DatabaseConnectionManager.java
@@ -1,5 +1,31 @@
 package com.edutech.progressive.config;
 
-public class DatabaseConnectionManager {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
 
+import org.springframework.stereotype.Component;
+
+import java.io.*;
+
+@Component
+public class DatabaseConnectionManager {
+    private static Properties properties = new Properties();
+
+    private static void loadProperties() throws IOException {
+        properties.load(new FileInputStream("/home/coder/app/server/src/main/resources/application.properties"));
+    }
+
+    public static Connection getConnection() throws SQLException {
+        try {
+            loadProperties();
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+        String url = properties.getProperty("spring.datasource.url");
+        String username = properties.getProperty("spring.datasource.username");
+        String password = properties.getProperty("spring.datasource.password");
+        return DriverManager.getConnection(url, username, password);
+    }
 }
