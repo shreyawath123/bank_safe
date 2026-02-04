714f132ee7eb649b3457848687af1236160fef60
shreyawath123 <shreyawath123@gmail.com>
2026-02-04 12:04:55 +0530

    Update BankSafeApplication.java

--- server/src/main/java/com/edutech/progressive/BankSafeApplication.java
+++ server/src/main/java/com/edutech/progressive/BankSafeApplication.java
@@ -1,7 +1,24 @@
 package com.edutech.progressive;
 
+import java.sql.SQLException;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class BankSafeApplication {
-    public static void main(String[] args) {
-        System.out.println("Welcome to BankSafe Project!");
+
+    public static void main(String[] args) throws SQLException {
+        // Connection conn = DatabaseConnectionManager.getConnection();
+        // System.out.println("Welcome to BankSafe Project!");
+        // System.out.println(conn);
+        // AccountDAOImpl accountDao = new AccountDAOImpl();
+        // AccountService as = new AccountServiceImpl(accountDao);
+        // CustomerDAOImpl customerDAO = new CustomerDAOImpl();
+        // CustomerService cs = new CustomerServiceImpl(customerDAO);
+        // Accounts account = new Accounts(22, 1111);
+        // Customers customer = new Customers("abs", "DD@gmail.com", "ddabs",
+        // "password");
+        SpringApplication.run(BankSafeApplication.class, args);
     }
 }
