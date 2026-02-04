7c4223d06ebf91f1d804b8d84a92e614c874a777
shreyawath123 <shreyawath123@gmail.com>
2026-02-04 11:21:28 +0530

    Update Customers.java

--- server/src/main/java/com/edutech/progressive/entity/Customers.java
+++ server/src/main/java/com/edutech/progressive/entity/Customers.java
@@ -1,5 +1,94 @@
 package com.edutech.progressive.entity;
 
-public class Customers {
+import javax.persistence.*;
+
+@Entity
+public class Customers implements Comparable<Customers> {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int customerId;
+    private String name;
+    private String email;
+    private String username;
+    private String password;
+    private String role;
+
+    public Customers() {
+    }
+
+    public Customers(String name, String email, String username, String password) {
+        this.name = name;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+    }
+
+    public Customers(int customerId, String name, String email, String username, String password) {
+        this.customerId = customerId;
+        this.name = name;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+        this.role = null;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public int compareTo(Customers o) {
+        return this.getName().compareTo(o.getName());
+    }
+
+    @Override
+    public String toString() {
+        return "Customers [customerId=" + customerId + ", name=" + name + ", email=" + email + ", username=" + username
+                + ", password=" + password + ", role=" + role + "]";
+    }
 
 }
