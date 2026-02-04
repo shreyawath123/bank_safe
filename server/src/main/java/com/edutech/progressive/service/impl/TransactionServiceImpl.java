8692e9f1fec67aaa5b9a9270fbc0b5b30a874ed6
shreyawath123 <shreyawath123@gmail.com>
2026-02-04 11:32:52 +0530

    Update TransactionServiceImpl.java

--- server/src/main/java/com/edutech/progressive/service/impl/TransactionServiceImpl.java
+++ server/src/main/java/com/edutech/progressive/service/impl/TransactionServiceImpl.java
@@ -1,5 +1,52 @@
 package com.edutech.progressive.service.impl;
 
-public class TransactionServiceImpl {
-    
+import java.sql.SQLException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.edutech.progressive.dao.TransactionDAO;
+import com.edutech.progressive.entity.Transactions;
+import com.edutech.progressive.service.TransactionService;
+
+@Service
+public class TransactionServiceImpl implements TransactionService {
+    @Autowired
+    private TransactionDAO transactionDAO;
+
+    public TransactionServiceImpl(TransactionDAO transactionDAO) {
+        this.transactionDAO = transactionDAO;
+    }
+
+    @Override
+    public List<Transactions> getAllTransactions() throws SQLException {
+        return transactionDAO.getAllTransactions();
+    }
+
+    @Override
+    public Transactions getTransactionById(int transactionId) throws SQLException {
+        return transactionDAO.getTransactionById(transactionId);
+    }
+
+    @Override
+    public int addTransaction(Transactions transaction) throws SQLException {
+        return transactionDAO.addTransaction(transaction);
+    }
+
+    @Override
+    public void updateTransaction(Transactions transaction) throws SQLException {
+        transactionDAO.updateTransaction(transaction);
+    }
+
+    @Override
+    public void deleteTransaction(int transactionId) throws SQLException {
+        transactionDAO.deleteTransaction(transactionId);
+    }
+
+    @Override
+    public List<Transactions> getTransactionsByCustomerId(int customerId) throws SQLException {
+        return null;
+    }
+
 }
