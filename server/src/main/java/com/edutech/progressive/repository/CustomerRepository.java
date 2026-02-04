6b21252017587b67e7630da6d445249f4be912d8
shreyawath123 <shreyawath123@gmail.com>
2026-02-04 12:24:40 +0530

    Update CustomerRepository.java

--- server/src/main/java/com/edutech/progressive/repository/CustomerRepository.java
+++ server/src/main/java/com/edutech/progressive/repository/CustomerRepository.java
@@ -1,9 +1,6 @@
 package com.edutech.progressive.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.edutech.progressive.entity.Customers;
-
-public interface CustomerRepository extends JpaRepository<Customers,Integer> {
+public interface CustomerRepository  {
 
 }
