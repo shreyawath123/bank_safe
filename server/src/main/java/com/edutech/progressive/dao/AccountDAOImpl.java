fe808442bc9fceda0d204ade709459a0013b43a7
shreyawath123 <shreyawath123@gmail.com>
2026-02-04 11:23:21 +0530

    Update AccountDAOImpl.java

--- server/src/main/java/com/edutech/progressive/dao/AccountDAOImpl.java
+++ server/src/main/java/com/edutech/progressive/dao/AccountDAOImpl.java
@@ -1,5 +1,64 @@
 package com.edutech.progressive.dao;
 
-public class AccountDAOImpl {
+import java.sql.*;
+import java.util.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.stereotype.Repository;
+
+import com.edutech.progressive.config.DatabaseConnectionManager;
+import com.edutech.progressive.entity.Accounts;
+import com.edutech.progressive.repository.AccountRepository;
+
+@Repository
+public class AccountDAOImpl implements AccountDAO {
+    @Autowired
+    AccountRepository ar;
+    public Connection connection;
+
+    public AccountDAOImpl() {
+        try {
+            this.connection = DatabaseConnectionManager.getConnection();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public List<Accounts> getAllAccounts() {
+        return ar.findAll();
+    }
+
+    public List<Accounts> getAllAccountsByCustomer(int customer_id) {
+        return ar.findByCustomerId(customer_id);
+    }
+
+    @Override
+    public Accounts getAccountById(int accountId) {
+        return ar.findById(accountId).orElse(null);
+    }
+
+    @Modifying
+    @Override
+    public int addAccount(Accounts accounts) {
+        return ar.save(accounts).getAccountId();
+    }
+
+    @Modifying
+    @Override
+    public void updateAccount(Accounts accounts) {
+        if (ar.existsById(accounts.getAccountId())) {
+            ar.save(accounts);
+        }
+    }
+
+    @Modifying
+    @Override
+    public void deleteAccount(int accountId) {
+        if (ar.existsById(accountId)) {
+            ar.deleteById(accountId);
+        }
+    }
 
 }
